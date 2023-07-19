c62f3cbae60ee2c058dd072a95b73747251f95e7
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-07-19 09:59:22 +0200

    added webclient

---
+++ src/main/java/no/fintlabs/RestUtil.java
@@ -0,0 +1,23 @@
+package no.fintlabs;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Service
+public class RestUtil {
+
+    private final WebClient webClient;
+
+    public RestUtil(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public <T> Mono<T> get(Class<T> clazz, String uri){
+        return webClient.get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(clazz);
+    }
+
+}

f5a9ba0aed7542db93790f8b41068371281f2b67
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2023-07-20 12:14:58 +0200

    add exists filter

--- src/main/java/no/fintlabs/RestUtil.java
+++ src/main/java/no/fintlabs/RestUtil.java
@@ -6,18 +6,27 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class RestUtil {
-
     private final WebClient webClient;
 
     public RestUtil(WebClient webClient) {
         this.webClient = webClient;
     }
 
-    public <T> Mono<T> get(Class<T> clazz, String uri){
+    public <T> Mono<T> get(Class<T> clazz, String uri) {
         return webClient.get()
                 .uri(uri)
                 .retrieve()
                 .bodyToMono(clazz)
                 .log();
+    }
+
+    public boolean exists(String endpoint) {
+        try {
+            Mono<Void> responseMono = webClient.get().uri(endpoint)
+                    .retrieve().bodyToMono(Void.class);
+            return false;
+        } catch (Exception e) {
+            return true;
+        }
     }
 }
--- src/main/java/no/fintlabs/TextClient.java
+++ src/main/java/no/fintlabs/TextClient.java
@@ -1,8 +1,7 @@
 package no.fintlabs;
 
-
 import lombok.extern.slf4j.Slf4j;
-import no.fint.model.resource.utdanning.utdanningsprogram.SkoleResources;
+import no.fint.model.resource.utdanning.ot.OTUngdomResources;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -10,6 +9,7 @@ import javax.annotation.PostConstruct;
 @Slf4j
 @Service
 public class TextClient {
+    public String endpoint = "utdanning/ot/otungdom";
     private final RestUtil restUtil;
 
     public TextClient(RestUtil restUtil) {
@@ -18,8 +18,13 @@ public class TextClient {
 
     @PostConstruct
     private void fetchData() {
-        restUtil.get(SkoleResources.class, "https://beta.felleskomponent.no/utdanning/utdanningsprogram")
-                .doOnNext(l -> log.info(String.valueOf(l)))
-                .subscribe();
+        if (restUtil.exists(endpoint)) {
+            restUtil.get(OTUngdomResources.class, endpoint)
+                    .doOnNext(l -> log.info(String.valueOf(l)))
+                    .doOnError(e -> log.error("Something went wrong: "))
+                    .subscribe();
+        }
+        else{log.error("This endpoint dose not exist");
+        }
     }
 }
